dd6617a71977edf967b2d81a31d13212ee770370
Edmund Tang <98913678+EdmundLT@users.noreply.github.com>
2023-04-07 15:20:32 -0400

    lt

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDF7gGhlKBy0OnwPGM6HcKpGWLTMPT7GdE',
+    authDomain: 'hireme-52683.firebaseapp.com',
+    projectId: 'hireme-52683',
+    storageBucket: 'hireme-52683.appspot.com',
+    messagingSenderId: '712051581992',
+    appId: '1:712051581992:web:76bc1e84dea6fce60d811d',
+    measurementId: 'G-1V3FLX9FJZ',
+  },
 };
